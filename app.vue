5181b877d24c118517325e6cf9c109739feb2cbf
Vjacheslav Trushkin <cyberalien@gmail.com>
2022-02-25 11:52:43 +0200

    Bug demo

---
+++ app.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<Test />
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, h } from 'vue';
+
+const Test = defineComponent({
+	data() {
+		return {
+			text: 'default text',
+		};
+	},
+
+	// Does not work!
+	beforeMount() {
+		this.text = 'component is about to be mounted';
+	},
+
+	// Works!
+	/*
+	mounted() {
+		this.text = 'component mounted';
+	},
+  */
+
+	render() {
+		return h('span', {
+			innerHTML: this.text,
+		});
+	},
+});
+
+export default {
+	components: {
+		Test,
+	},
+};
+</script>
